f8ef56e0164e6e29cb7e732e2c31ded7b4ab3826
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-09-29 21:38:31 +0300

    increase performance of generating random string

--- shortener/randStr/randStr.go
+++ shortener/randStr/randStr.go
@@ -22,21 +22,36 @@ import (
 const (
 	//letterNumberBytes is the available chars that can be used in the shortened url.
 	letterNumberBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// chanSize is the size of strChan channel. RandStr will generate strings up to this number and they will be consumed
+	// by Next() method.
+	chanSize = 300
 )
 
 type RandStr struct {
-	n int
+	n       int
+	strChan chan string
 }
 
 func New(n int) *RandStr {
 	rand.Seed(time.Now().UTC().UnixNano())
-	return &RandStr{n: n}
+	r := &RandStr{n: n, strChan: make(chan string, chanSize)}
+	go r.generate()
+	return r
+}
+
+func (r *RandStr) generate() {
+	// TODO:: break this loop when server shutdowns.
+	for {
+		b := make([]byte, r.n)
+		for i := range b {
+			b[i] = letterNumberBytes[rand.Intn(len(letterNumberBytes))]
+		}
+		r.strChan <- string(b)
+	}
+
 }
 
 func (r *RandStr) Next() string {
-	b := make([]byte, r.n)
-	for i := range b {
-		b[i] = letterNumberBytes[rand.Intn(len(letterNumberBytes))]
-	}
-	return string(b)
+	return <-r.strChan
 }
